3a695d367d1ce32a0d7e38af1bd3df28a15e414d
Anu S Kumar <64139878+ASK369001@users.noreply.github.com>
2025-01-22 06:10:13 +0000

    Carousel view added

--- src/views/HomePage.jsx
+++ src/views/HomePage.jsx
@@ -1,10 +1,12 @@
 import React from "react";
 import Header from "../components/header/Header";
+import CarouselView from "../components/carousel/CarouselView";
 
 const HomeView = () => {
   return (
     <div className="home-container">
       <Header />
+      <CarouselView />
     </div>
   );
 };
